d99eeac6c3a038e7268724956dbf1d8f1da27ef4
Ala mostafa <Almoaalba1102@gmail.com>
2022-02-14 16:27:51 +0200

    \

--- js/planets.js
+++ js/planets.js
@@ -16,23 +16,44 @@ function something ()
 
 let activeDiv = (e) => {
     const observer = new IntersectionObserver((entries)=> {
-        console.log(entries)
         entries.forEach(entry =>{
             if(entry.isIntersecting)
             {
                 
                 entry.target.classList.add('active');
-                entry.target.insertAdjacentElement("afterbegin",img)
+        
             }
             else
             {
                 entry.target.classList.remove('active')
-                entry.target.removeChild(img)
             }
         })
 
-    }, {threshold: [0.8],});
+    }, {threshold: [0.7],});
     observer.observe(e);
 }
 const sections = document.querySelectorAll('section')
 sections.forEach(section => activeDiv(section) )
+
+//To the top
+const toTheTop =document.getElementById('toTop');
+document.addEventListener('scroll', function (){
+
+    // to make the button appear while scrolling to a specific limit -550-
+    if (window.scrollY >= '550')
+    {
+        toTheTop.style.display = 'block';
+    
+        //to make the button go to the top when is clicked
+        toTheTop.addEventListener('click', function () {
+            window.scrollTo(0,0);
+        });
+        
+    }
+    // to make the button disappear while scrolling to a limit smaller than 550
+    else
+    {
+        toTheTop.style.display = 'none';
+    }
+    });
+    
